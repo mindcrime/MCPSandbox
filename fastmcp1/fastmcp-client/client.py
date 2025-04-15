85e3728235cea4ae03a5359ff08ec87bed494d9d
Phillip Rhodes <motley.crue.fan@gmail.com>
2025-04-15 19:27:14 -0400

    working http/sse end-to-end example

--- fastmcp1/fastmcp-client/client.py
+++ fastmcp1/fastmcp-client/client.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
 
-def main():
+import asyncio
+from fastmcp import Client, FastMCP
+
+async def main():
 
     print( "Hello, FastMCP Client!\n" )
+    sse_url = "http://localhost:8000/sse"       # SSE server URL
+    client = Client(sse_url)
 
+    print( client.transport )
+
+    # Connection is established here
+    async with client:
+        print(f"Client connected: {client.is_connected()}")
+
+        # Make MCP calls within the context
+        tools = await client.list_tools()
+        print(f"Available tools: {tools}")
+
+
+        if any(tool.name == "multiply" for tool in tools):
+            result = await client.call_tool("multiply", {"a": 3, "b":4})
+            print(f"Greet result: {result}")
+
+    # Connection is closed automatically here
+    print(f"Client connected: {client.is_connected()}")
+
+
+        
+    print( "Done" )
+    
+    
 if __name__ == "__main__":
 
-    main()
+    asyncio.run( main() )
     
--- fastmcp1/fastmcp-server/server.py
+++ fastmcp1/fastmcp-server/server.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 
+from fastmcp import FastMCP
+
+
 def main():
 
     print( "Hello, FastMCP Server!\n" )
 
+    # Create a basic server instance
+    mcp = FastMCP(name="MyMCPServer",
+                  instructions="This server provides data analysis tools. Call multiply() to multiply two integers" )
+
+    # print( dir ( mcp ) )
+    
+    
+    @mcp.tool()
+    def multiply( a: int, b: int ) -> int:
+       """multiply - multiplies two integers and returns an integer"""
+       return a*b
+
+    
+    mcp.run(transport="sse")
+
+   
 if __name__ == "__main__":
 
     main()
